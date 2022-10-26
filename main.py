967d42a36d4a7f05c9a9a68a303bd90382dc727e
Shrey <skansal@ucsd.edu>
2022-10-26 09:39:33 -0700

    submit

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import numpy as np
 
 
 def plot_data(data_, alpha_):
-    plt.plot(data_[:,0], data_[:,1], label=("Frequency: " + str(30/alpha_) + " hz"))
+    plt.plot(data_[:,0], data_[:,1], label=("Interpolated from " + str(30/alpha_) + " hz to 30Hz"))
     plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.show(block=True)
     return
 
 def downsample_data(data_, alpha_):
@@ -12,9 +15,9 @@ def downsample_data(data_, alpha_):
     return downsampled_data_
 
 
-def lin_int(orig_data_, int_data_):
+def lin_int(int_data_, alpha_):
     wp = []
-    alpha_ = round(orig_data_.shape[0]/int_data_.shape[0])
+
     for i in range(0, int_data_.shape[0]-1):
         f0 = int_data_[i]
         wp.append(f0)
@@ -28,9 +31,8 @@ def lin_int(orig_data_, int_data_):
     wp.append(int_data_[-1])
     return np.array(wp)
 
-def quad_int(orig_data_, int_data_):
+def quad_int(int_data_, alpha_):
     wp  =[]
-    alpha_ = round(orig_data_.shape[0] / int_data_.shape[0])
 
     for i in range(0, int_data_.shape[0]-2, 2):
         t0 = i*alpha_
@@ -58,9 +60,8 @@ def quad_int(orig_data_, int_data_):
 
     return  np.array(wp)
 
-def cub_int(orig_data_, int_data_):
+def cub_int(int_data_, alpha_):
 
-    alpha_ = round(orig_data_.shape[0]/int_data_.shape[0])
     n = int_data_.shape[0] - 1
     A = 2*np.eye(n+1)
     dx = np.zeros((n+1,1))     #d[0] = d[n] = 0
@@ -103,8 +104,9 @@ def cub_int(orig_data_, int_data_):
         dy[i] = 6*(Ly0 + Ly1 + Ly2)
 
     Mx = np.linalg.inv(A) @ dx
+    
     My = np.linalg.inv(A) @ dy
-
+    np.savetxt('My.csv', My)
     wp = []
     
     for i in range(1,n):
@@ -140,45 +142,46 @@ if __name__ == "__main__":
     
     waypoints = np.loadtxt("./waypoints.csv", delimiter=",")
 
-    # Get Downsampled Data
-    data_10hz = downsample_data(waypoints, 3)
-    data_01hz = downsample_data(waypoints, 30)
-    data_p2hz = downsample_data(waypoints, 150)
+    ## Get Downsampled Data
+    alpha_10 = 3
+    alpha_01 = 30
+    alpha_p2 = 150
+    data_10hz = downsample_data(waypoints, alpha_10)
+    data_01hz = downsample_data(waypoints, alpha_01)
+    data_p2hz = downsample_data(waypoints, alpha_p2)
+
+    ## GROUND TRUTH
+    plot_data(waypoints,1)
 
     ## LINEAR INTERPOLATION
-    lin_10hz = lin_int(waypoints, data_10hz)
-    lin_01hz = lin_int(waypoints, data_01hz)
-    lin_p2hz = lin_int(waypoints, data_p2hz)
+    lin_10hz = lin_int(data_10hz, alpha_10)
     print(get_error(waypoints, lin_10hz))
-    print(get_error(waypoints, lin_01hz))
-    print(get_error(waypoints, lin_p2hz))
-    plot_data(waypoints, 1)
     plot_data(lin_10hz, 3)
+    lin_01hz = lin_int(data_01hz, alpha_01)
+    print(get_error(waypoints, lin_01hz))
     plot_data(lin_01hz, 30)
+    lin_p2hz = lin_int(data_p2hz, alpha_p2)
+    print(get_error(waypoints, lin_p2hz))
     plot_data(lin_p2hz, 150)
 
     ## QUADRATIC INTERPOLATION
-    quad_10hz = quad_int(waypoints, data_10hz)
-    quad_01hz = quad_int(waypoints, data_01hz)
-    quad_p2hz = quad_int(waypoints, data_p2hz)
+    quad_10hz = quad_int(data_10hz, alpha_10)
     print(get_error(waypoints, quad_10hz))
-    print(get_error(waypoints, quad_01hz))
-    print(get_error(waypoints, quad_p2hz))
-    plot_data(waypoints, 1)
     plot_data(quad_10hz, 3)
+    quad_01hz = quad_int(data_01hz, alpha_01)
+    print(get_error(waypoints, quad_01hz))
     plot_data(quad_01hz, 30)
+    quad_p2hz = quad_int(data_p2hz, alpha_p2)
+    print(get_error(waypoints, quad_p2hz))
     plot_data(quad_p2hz, 150)
 
     ## CUBIC INTERPOLATION
-    cub_10hz = cub_int(waypoints, data_10hz)
-    cub_01hz = cub_int(waypoints, data_01hz)
-    cub_p2hz = cub_int(waypoints, data_p2hz)
+    cub_10hz = cub_int(data_10hz, alpha_10)
     print(get_error(waypoints, cub_10hz))
-    print(get_error(waypoints, cub_01hz))
-    print(get_error(waypoints, cub_p2hz))
-    plot_data(waypoints, 1)
     plot_data(cub_10hz, 3)
+    cub_01hz = cub_int(data_01hz, alpha_01)
+    print(get_error(waypoints, cub_01hz))
     plot_data(cub_01hz, 30)
+    cub_p2hz = cub_int(data_p2hz, alpha_p2)
+    print(get_error(waypoints, cub_p2hz))
     plot_data(cub_p2hz, 150)
-
-    plt.show(block=True)
